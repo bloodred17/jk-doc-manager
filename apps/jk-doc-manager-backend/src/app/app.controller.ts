3e2f65efc708607d8d7a8c57ff06795716f5f1c3
Ankur Dutta <ankur.611@gmail.com>
2024-12-02 15:36:29 +0000

    File upload and file deletion endpoint created

--- apps/jk-doc-manager-backend/src/app/app.controller.ts
+++ apps/jk-doc-manager-backend/src/app/app.controller.ts
@@ -1,23 +1,88 @@
-import {Controller, Get, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FirebaseService } from './firebase.service';
-import {FileInterceptor} from "@nestjs/platform-express";
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FirebaseAuthGuard } from './firebase-auth.guard';
+import { getStorage } from 'firebase-admin/storage';
 
 @Controller()
 export class AppController {
   constructor(private readonly firebaseService: FirebaseService) {}
 
   @Get()
-  getData() {
+  check() {
     return {
-      message: this.firebaseService.getStorage(),
+      message: 'API is working.',
     };
   }
 
   @Post('upload')
+  @UseGuards(FirebaseAuthGuard)
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: File) {
-    return {
-      result: await this.firebaseService.uploadToFirebase(file, 'abc.pdf')
+  async uploadFile(@UploadedFile() file: File, @Query('name') name: string) {
+    try {
+      if (!name) {
+        throw new Error('Name is required');
+      }
+      return {
+        success: true,
+        data: await this.firebaseService.uploadToFirebase(file, name),
+        description: 'File uploaded successfully',
+      };
+    } catch (e) {
+      return {
+        success: false,
+        error: e?.toString(),
+      };
+    }
+  }
+
+  @Get('files')
+  @UseGuards(FirebaseAuthGuard)
+  async getFile() {
+    try {
+      const bucket = getStorage().bucket();
+      const files = await bucket.getFiles();
+      return {
+        success: true,
+        data: files[0].map((file) => ({
+          name: file.name,
+          url: file.publicUrl(),
+        })),
+      };
+    } catch (e) {
+      return {
+        success: false,
+        error: e?.toString(),
+      };
+    }
+  }
+
+  @Delete('file/:name')
+  @UseGuards(FirebaseAuthGuard)
+  async deleteFile(@Param('name') filename: string) {
+    try {
+      const fileName = decodeURIComponent(filename);
+      const file = getStorage().bucket().file(fileName);
+      const result = await file.delete();
+      return {
+        success: true,
+        data: result,
+      };
+    } catch (e) {
+      return {
+        success: false,
+        error: e?.toString(),
+      };
     }
   }
 }
